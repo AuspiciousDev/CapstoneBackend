137591fa60eacabf325466312d1d84be9b7c12a2
Auspicious <106650462+AuspiciousDev@users.noreply.github.com>
2022-11-30 11:56:58 +0800

    Update allowedOrigins.js

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -2,7 +2,7 @@ const allowedOrigins = [
   "https://aus-caps-portal.onrender.com",
   "https://aus-caps-portal.onrender.com/",
   "http://127.0.0.1:3600",
-  "http://localhost:3600/",
+  "http://localhost:3600",
   "http://localhost:3000/",
   "http://localhost:4000/",
 ];
