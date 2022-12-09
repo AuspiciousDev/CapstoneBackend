59e5228a3a91a3dac73ab2bbbb247d0c342a5537
Auspicious <106650462+AuspiciousDev@users.noreply.github.com>
2022-12-09 12:43:20 +0800

    Update enrolledController.js

--- server/controller/enrolledController.js
+++ server/controller/enrolledController.js
@@ -51,61 +51,69 @@ const getAllDoc = async (req, res) => {
   res.status(200).json(doc);
 };
 const createDoc = async (req, res) => {
-  // Retrieve data
-  const {
-    enrollmentID,
-    schoolYearID,
-    studID,
-    levelID,
-    sectionID,
-    departmentID,
-  } = req.body;
-
-  console.log("New Enrollee: ", req.body);
-  // Validate Data if given
-  if (
-    !enrollmentID ||
-    !schoolYearID ||
-    !studID ||
-    !levelID ||
-    !sectionID ||
-    !departmentID
-  ) {
-    return res.status(400).json({ message: "All Fields are required!" });
-  }
-  // Check for Duplicate Data
-  const duplicate = await Enrolled.findOne({
-    studID: studID,
-    schoolYearID: schoolYearID,
-  })
-    .lean()
-    .exec();
-  if (duplicate)
-    return res.status(409).json({ message: "Duplicate Enrollee!" });
-  const isActive = await Student.findOne({
-    studID: studID,
-  });
-  if (!isActive)
-    return res.status(409).json({ message: "Student does not exists!!" });
-  if (isActive.status === false)
-    return res.status(409).json({ message: "Student not Active!" });
-  // Create Object
-  const docObject = {
-    enrolledID: enrollmentID,
-    schoolYearID,
-    studID,
-    levelID,
-    sectionID,
-    departmentID,
-  };
-  // Create and Store new Doc
   try {
-    // const empObjectRes = await Employee.create(empObject);
-    const response = await Enrolled.create(docObject);
-    res.status(201).json(response);
+    const {
+      enrollmentID,
+      schoolYearID,
+      studID,
+      levelID,
+      sectionID,
+      departmentID,
+    } = req.body;
+
+    console.log("New Enrollee: ", req.body);
+    // Validate Data if given
+    if (
+      !enrollmentID ||
+      !schoolYearID ||
+      !studID ||
+      !levelID ||
+      !sectionID ||
+      !departmentID
+    ) {
+      return res.status(400).json({ message: "All Fields are required!" });
+    }
+    // Check for Duplicate Data
+    const duplicate = await Enrolled.findOne({
+      studID: studID,
+      schoolYearID: schoolYearID,
+    })
+      .lean()
+      .exec();
+    if (duplicate)
+      return res.status(409).json({ message: "Duplicate Enrollee!" });
+    const isActive = await Student.findOne({
+      studID: studID,
+    });
+    if (!isActive)
+      return res.status(409).json({ message: "Student does not exists!!" });
+    if (isActive.status === false)
+      return res.status(409).json({ message: "Student is not Active!" });
+    // Create Object
+    const docObject = {
+      enrolledID: enrollmentID,
+      schoolYearID,
+      studID,
+      levelID,
+      sectionID,
+      departmentID,
+    };
+    // Create and Store new Doc
+    try {
+      // const empObjectRes = await Employee.create(empObject);
+      const response = await Enrolled.create(docObject);
+      res.status(201).json(response);
+    } catch (error) {
+      console.error(error);
+    }
   } catch (error) {
-    console.error(error);
+    console.log(
+      "🚀 ~ file: enrolledController.js:110 ~ createDoc ~ error",
+      error
+    );
+    res.status(500).json({ message: error.message });
   }
+  // Retrieve data
 };
 
 const getDocByID = async (req, res) => {
@@ -171,7 +179,7 @@ const deleteDocByID = async (req, res) => {
     if (!findID) {
       return res.status(400).json({ message: `${enrolledID} not found!` });
     }
-    const findGrade = await Grade.findOne({ enrolledID }).exec();
+    const findGrade = await Grade.findOne({ studID }).exec();
     console.log("Enrolled FindGrade: ", findGrade);
     if (findGrade) {
       return res.status(400).json({
@@ -192,35 +200,43 @@ const deleteDocByID = async (req, res) => {
   }
 };
 const toggleStatusById = async (req, res) => {
-  console.log("Enrolled Toggle :", req.body);
-  const { enrolledID, studID, schoolYearID, status } = req.body;
-  if (!enrolledID || !studID || !schoolYearID) {
-    return res.status(400).json({ message: "ID required to toggle status!" });
-  }
-
-  const findID = await Enrolled.findOne({
-    enrolledID,
-    studID,
-    schoolYearID,
-  }).exec();
-  if (!findID) {
-    return res
-      .status(400)
-      .json({ message: `${studID} not found in year ${schoolYearID} !` });
-  }
-  const updateItem = await Enrolled.findOneAndUpdate(
-    { enrolledID: enrolledID },
-    {
-      status,
+  try {
+    console.log("Enrolled Toggle :", req.body);
+    const { enrolledID, studID, schoolYearID, status } = req.body;
+    if (!enrolledID || !studID || !schoolYearID) {
+      return res.status(400).json({ message: "ID required to toggle status!" });
     }
-  );
 
-  if (!updateItem) {
-    return res.status(400).json({ message: "No Student" });
+    const findID = await Enrolled.findOne({
+      enrolledID,
+      studID,
+      schoolYearID,
+    }).exec();
+    if (!findID) {
+      return res
+        .status(400)
+        .json({ message: `${studID} not found in year ${schoolYearID} !` });
+    }
+    const updateItem = await Enrolled.findOneAndUpdate(
+      { enrolledID: enrolledID },
+      {
+        status,
+      }
+    );
+
+    if (!updateItem) {
+      return res.status(400).json({ message: "No Student" });
+    }
+    // const result = await response.save();
+    console.log("Enrolled update : ", updateItem);
+    res.json(updateItem);
+  } catch (error) {
+    console.log(
+      "🚀 ~ file: enrolledController.js:234 ~ toggleStatusById ~ error",
+      error
+    );
+    res.status(500).json({ message: error.message });
   }
-  // const result = await response.save();
-  console.log("Enrolled update : ", updateItem);
-  res.json(updateItem);
 };
 module.exports = {
   createDoc,
